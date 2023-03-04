f7cc2a934d88ddcd416fa7d63ff438814574995c
Vishal7489 <salujavishal370@gmail.com>
2023-03-04 13:29:08 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Work from "./components/Work";
+import Timeline from "./components/Timeline";
+import Services from "./components/Services"; 
+import Testimonial from "./components/Testimonial";
+import {Toaster} from "react-hot-toast";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Work/>
+    <Timeline/>
+    <Services/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return <nav>
+      <NavContent/>
+    </nav>
+}
+const NavContent = ()=>(
+<>
+<h2>Vishal.</h2>
+<div> 
+<a href="#home">Home </a>
+<a href="#work">Work </a>
+<a href="#timeline">Experience </a>
+<a href="#services">Services </a>
+<a href="#testimonial">Testimonial</a>
+<a href="#contact">Contact </a>
+</div>
+<a href="mailto:official.salujavishal370@gmail.com"><button>Email</button></a>
+</>
+);
+
+export default Header;
